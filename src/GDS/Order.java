c21eacb0c1f5870a1be3fce36e4795bba45e8875
FOTSING KENGNE Junior <FOTSING KENGNE Junior>
2016-04-02 10:06:56 +0200

    modification of class "Order" because the variables are not complying with set standarts

--- src/GDS/Order.java
+++ src/GDS/Order.java
@@ -5,22 +5,22 @@ import java.util.ArrayList;
 
 public class Order {
 	
-	private long odr_id;
-	private double odr_price;
-	private int odr_discountPct;
-	private String odr_clientName;
-	private String odr_state;
-	private Date odr_date;
+	private long id;
+	private double price;
+	private int discountPct;
+	private String clientName;
+	private String state;
+	private Date date;
 	private ArrayList<Long> productIdList;
 	
-	public Order(long odr_id, double odr_price, int odr_discountPct, String odr_clientName, 
-			String odr_state, Date odr_date, ArrayList<Long> productIdList) {
-		this.odr_clientName = odr_clientName;
-		this.odr_date = odr_date;
-		this.odr_discountPct = odr_discountPct;
-		this.odr_id = odr_id;
-		this.odr_price = odr_price;
-		this.odr_state = odr_state;
+	public Order(long id, double price, int discountPct, String clientName, 
+			String state, Date date, ArrayList<Long> productIdList) {
+		this.clientName = clientName;
+		this.date = date;
+		this.discountPct = discountPct;
+		this.id = id;
+		this.price = price;
+		this.state = state;
 		this.productIdList = productIdList;
 	}
 
