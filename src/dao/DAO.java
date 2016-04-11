6a91e63001e00f1737061824b3f4e5b61d6403fd
FOTSING KENGNE Junior <FOTSING KENGNE Junior>
2016-04-11 21:13:08 +0200

    no message

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -11,20 +11,21 @@ abstract class DAO {
 	 * connection parameter between oracle URL and the DGB, LOGIN and PASS are
 	 * constants
 	 */
-	/*
+	
 	final static String URL = "jdbc:oracle:thin:@localhost:1521:xe"; 
 	final static String LOGIN = "system";
 	final static String PASS = "bdd";
-	 */
+	
 	/*
 	final static String URL = "jdbc:oracle:thin:@localhost:1521:xe";
 	final static String LOGIN = "BDD6"; 
 	final static String PASS = "BDD6";
 	 */
+	/*
 	final static String URL = "jdbc:oracle:thin:@localhost:1521:dbkun";
 	final static String LOGIN = "c##nathankun";
 	final static String PASS = "83783548jun";
-
+	*/
 	/**
 	 * Constructor
 	 */
